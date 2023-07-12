19e3a86caf011278ac3e30a7733253a1442c80e1
Todd H. Albert <todd@bocacode.com>
2023-07-12 12:45:10 -0400

    began building imdb clone

---
+++ app/about/anthony/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main className="p-8">
+      <h1 className="text-2xl font-semibold text-orange-400">About Anthony</h1>
+      <p className="mt-4 text-lg text-orange-200">It's my birthday, yo!</p>
+    </main>
+  )
+}
